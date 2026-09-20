6bb7d15542e7ed139fa0e2547fb864b021eec6b9
George Ekpo <gekpo4@byupathway.edu>
2026-09-20 07:40:11 +0100

    week03 country page

---
+++ scripts/place.js
@@ -0,0 +1,28 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const temperature = 10;
+const windSpeed = 12;
+
+function calculateWindChill(temperature, windSpeed) {
+    return 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
+}
+
+function showWindChill() {
+    const windChillDiv = document.getElementById("windchill");
+
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const result = calculateWindChill(temperature, windSpeed);
+        windChillDiv.textContent = Math.round(result) + " °C";
+    } else {
+        windChillDiv.textContent = "N/A";
+    }
+}
+
+showWindChill();
+
+const temp = document.getElementById("temperature");
+temp.textContent = temperature + " ℃";
+
+const wind = document.getElementById("wind");
+wind.textContent = windSpeed + " km/h";
